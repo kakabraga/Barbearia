6624f3c12bd732b34ac98c0ab2dace280c666f27
kakabraga <167010103+kakabraga@users.noreply.github.com>
2024-08-09 22:07:36 -0300

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -11,11 +11,3 @@
     <a class="navbar-brand" href="#">Navbar</a>
   </div>
 </nav>
-
-<!-- As a heading -->
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <span class="navbar-brand mb-0 h1">Navbar</span>
-  </div>
-</nav>
-</body>
