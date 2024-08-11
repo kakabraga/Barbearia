fde632e6f5c5d1191777a518f695888397ad6cc0
kakabraga <167010103+kakabraga@users.noreply.github.com>
2024-08-10 22:10:26 -0300

    Create save_clientes.php

---
+++ save_clientes.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require 'db.php';
+if (isset($_POST['save_cliente'])) {
+    $nome = mysqli_real_escape_string($db_barbearia, trim($_POST['nome']));
+    $telefone = mysqli_real_escape_string($db_barbearia, trim($_POST['telefone']));
+    $cep = mysqli_real_escape_string($db_barbearia, trim($_POST['cep']));
+    $email = mysqli_real_escape_string($db_barbearia, trim($_POST['email']));
+    $cpf = mysqli_real_escape_string($db_barbearia, trim($_POST['cpf']));
+
+    $sql = "INSERT INTO cliente (nome, telefone, cep, email, cpf) values ('$nome', '$telefone', '$cep', '$email', '$cpf')";
+
+    mysqli_query($db_barbearia, $sql);
+}
