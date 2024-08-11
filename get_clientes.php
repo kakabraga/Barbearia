b1fa76ac0cba763a347824e5619eb3b59e52b801
kakabraga <167010103+kakabraga@users.noreply.github.com>
2024-08-10 21:01:29 -0300

    Update get_clientes.php

--- get_clientes.php
+++ get_clientes.php
@@ -7,12 +7,16 @@ $clientes = mysqli_query($db_barbearia, $sql);
 if (mysqli_num_rows($clientes) > 0) {
     foreach($clientes as $cliente) {
         echo"<tr>";
-        echo"<td>" .$cliente['id']. "</td>";
-        echo"<td>" .$cliente['nome']. "</td";
-        echo"<td>" .$cliente['telefone']. "</td";
-        echo"<td>" .$cliente['email']. "</td";
-        echo"<td>" .$cliente['cep']. "</td";
-        echo"<td>" .$cliente['cpf']. "</td";
+        echo"  <td>".$cliente['id']."</td>";
+        echo"  <td>".$cliente['nome']."</td>";
+        if ($cliente['telefone'] <= 0) {
+            echo "<td>Sem numero registrado</td>";
+        } else {
+            echo"<td>".$cliente['telefone']."</td>";
+        }
+        echo"  <td>".$cliente['email']."</td>";
+        echo"  <td>".$cliente['cep']."</td>";
+        echo"  <td>".$cliente['cpf']."</td>";
         echo"</tr>";
     }
 }
