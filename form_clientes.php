e7d2dfb9fec462815c0cf9f294d0325ebbb98b42
kakabraga <167010103+kakabraga@users.noreply.github.com>
2024-08-10 21:14:03 -0300

    Create form_clientes.php

---
+++ form_clientes.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário de Clientes</title>
+</head>
+<body>
+    <?php include 'navbar.php'?>
+    <div class="container mt-4">
+    <form>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Nome</label>
+        <input type="text" class="form-control">
+    </div>
+</form>
+</body>
+    </div>
