807d7b724adbc03cbb04cc3642236afc38bb40fd
kakabraga <167010103+kakabraga@users.noreply.github.com>
2024-09-05 22:20:32 -0300

    delete

--- Barbearia/del_clientes.php
+++ Barbearia/del_clientes.php
@@ -2,11 +2,11 @@
 require 'db.php';
 if (isset($_POST['Excluir'])) {
     $id_cliente = mysqli_real_escape_string($db_barbearia, $_POST['Excluir']);
-    $sql = "DELETE from cliente where id ='$id_cliente'";
+    $sql = "DELETE from cliente where id = $id_cliente";
     if (mysqli_query($db_barbearia, $sql)) {
-        echo "Removido";
         header('Location: gerenciador_clientes.php');
     } else {
         echo "Erro ao excluir cliente: " . mysqli_error($db_barbearia);
     }
 }
+
--- Barbearia/gerenciador_clientes.php
+++ Barbearia/gerenciador_clientes.php
@@ -9,24 +9,29 @@ require 'db.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script>
+$(document).ready(function() {
+    $('#clientes').DataTable();
+});
+</script>
 </head>
 <body>
   <?php include('navbar.php')?>
   <div class="container mt-4">
     <div class="row">
       <div class="col-md-12">
-        <div class="card">
-          <div class="card-header">
+      <div class="card">
+      <div class="card-header" style="border-radius: 10px;">
             <h4>Clientes cadastrados
             <a href="form_clientes.php" class="btn btn-primary float-end">Adicionar clientes</a>
             </h4>
-            <div>
-            <input type="seachr" style="border-radius: 7px;">
-        </div>
           </div>
           <div class="card-body">
-            <table class="table table-bordered table-striped">
-            <thead>
+            <table id="clientes" class="table table-bordered table-striped">
+             <thead>
     <tr>
       <th scope="col" style="text-align:center;">ID</th>
       <th scope="col" style="text-align:center;">NOME</th>
