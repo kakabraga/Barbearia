4229210f5d6027ccbe92a984d2e3f1ae137118a1
kakabraga <167010103+kakabraga@users.noreply.github.com>
2024-09-06 10:22:02 -0300

    Update db.sql

--- DB/db.sql
+++ DB/db.sql
@@ -14,3 +14,35 @@ create table servicos (
 	tipo varchar (45),
 	preco float(10,2)
 );
+
+create table compras (
+	id int primary key AUTO_INCREMENT,
+	data_compra TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+	id_cliente int,
+	id_servico int,
+	id_forma_pagamento int(10),
+	preco float(10,2)
+);
+
+create table forma_pagamento (
+	id int primary key AUTO_INCREMENT,
+	forma char(10)
+);
+
+INSERT INTO `forma_pagamento`(`id`, `forma`) VALUES (null,'CREDITO');
+INSERT INTO `forma_pagamento`(`id`, `forma`) VALUES (null,'DEBITO');
+INSERT INTO `forma_pagamento`(`id`, `forma`) VALUES (null,'DINHEIRO');
+
+
+ALTER TABLE compras 
+ADD CONSTRAINT fk_id_cliente 
+FOREIGN KEY (id_cliente) REFERENCES cliente(id);
+
+ALTER TABLE compras 
+ADD CONSTRAINT fk_id_servico
+FOREIGN KEY (id_servico) REFERENCES servicos (id);
+ALTER TABLE compras 
+ADD CONSTRAINT fk_id_forma_pagamento
+FOREIGN KEY (id_forma_pagamento) REFERENCES forma_pagamento (id);
+
+INSERT INTO compras values(null, null,6,1,5,15);
