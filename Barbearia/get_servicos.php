8f496eb35d6896893cd60ba018ca5faee66a8ef0
kakabraga <167010103+kakabraga@users.noreply.github.com>
2024-09-05 23:29:34 -0300

    Update get_servicos.php

--- Barbearia/get_servicos.php
+++ Barbearia/get_servicos.php
@@ -10,10 +10,12 @@ if (mysqli_num_rows($servicos) > 0) {
         echo "<td>".$servico['id']."</td>";
         echo "<td>".$servico['tipo']."</td>";
         echo "<td>".$servico['preco']."</td>";
-        echo "<td> <a href='form_servicos.php?=".$servico['id']."' class='btn btn-primary btn-sm'>Editar</a>
+        echo "<td> 
+                <a href='form_altera_servicos.php?id=".$servico['id']."' class='btn btn-primary btn-sm'>Editar</a>&nbsp;&nbsp;&nbsp;
                 <form action='del_servico.php' method='POST' style='display:inline;'>
-                <button type='submit' name='excluir' value='".$servico['id']."' class='btn btn-danger btn-sm'>Excluir</button>
-                </form></td>";
+                <button type='submit' name='excluir' value='".$servico['id']."' class='btn btn-danger btn-sm'>Excluir</button>&nbsp;&nbsp;&nbsp;
+                </form>
+                <a href='form_servicos.php' class='btn btn-primary btn-sm float-end' style='border-radius: 5px;'>Adicionar Serviço</a></td>";
         echo "</tr>";
     }
 }
